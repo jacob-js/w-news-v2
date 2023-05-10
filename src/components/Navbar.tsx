9a39e64cbebbede6b2334450b5d41fde5890c84e
Jacob Dev <mercihabam@gmail.com>
2023-05-10 12:56:40 +0200

    feat: create navbar component

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import { Button, Input } from '@mui/joy'
+import React, { useState } from 'react'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { useNavigate } from 'react-router-dom';
+import Wrapper from './Wrapper'
+
+function NavBar() {
+  const [showMobileSearch, setShowMobileSearch] = useState(false);
+  const navigate = useNavigate();
+
+  return (
+    <Wrapper>
+      <div className='flex justify-between items-center py-5 sm:py-10'>
+          <div className={`cursor-pointer ${showMobileSearch ? 'hidden sm:block': ''}`} onClick={() =>navigate('/')}>
+              <img src="/logo.png" alt="" className='w-12' />
+          </div>
+          <div className="hidden sm:block">
+            <Input startDecorator={<AiOutlineSearch />} placeholder='Enter a keyword to filter' variant="soft" size="lg" className="w-80" />
+          </div>
+          {
+            showMobileSearch ?
+            <div className="block sm:hidden w-full animate-grow">
+              <Input startDecorator={<AiOutlineSearch />} placeholder='Enter a keyword to filter' variant="soft" fullWidth autoFocus onBlur={() =>setShowMobileSearch(false)} />
+            </div>:
+            <div className="block sm:hidden">
+              <Button variant="soft" color='neutral' onClick={() =>setShowMobileSearch(true)}><AiOutlineSearch /></Button>
+            </div>
+          }
+      </div>
+    </Wrapper>
+  )
+};
+
+function withNavBar(Component: typeof React.Component){
+  return <>
+    <NavBar />
+    <Component />
+  </>
+}
+
+export default withNavBar;
