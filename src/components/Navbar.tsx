4fa213a7bfa36f9ec36551e97bdb24459abaeee3
Jacob Dev <mercihabam@gmail.com>
2023-05-11 11:19:14 +0200

    feat: configure redux

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Button, Input } from '@mui/joy'
-import React, { ReactElement, useState } from 'react'
+import { ReactElement, useState } from 'react'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { useNavigate } from 'react-router-dom';
 import Wrapper from './Wrapper'
---
+++ src/hooks/useAppSelector.ts
@@ -0,0 +1,9 @@
+import {
+    useSelector,
+    TypedUseSelectorHook,
+  } from 'react-redux'
+import { RootState } from '../store'
+  
+const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+
+export default useAppSelector;
