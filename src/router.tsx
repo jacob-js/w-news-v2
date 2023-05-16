71933377abe96d54e668c8a88c87d97cf86176ff
Jacob Dev <mercihabam@gmail.com>
2023-05-16 12:37:42 +0200

    feat: design not found page

--- src/router.tsx
+++ src/router.tsx
@@ -3,6 +3,7 @@ import withNavBar from "./components/Navbar";
 import Home from "./pages/Home";
 import ArticleDetails from "./pages/ArticleDetails";
 import PublisherArticles from "./pages/PublisherArticles";
+import NotFound from "./pages/NotFound";
 
 export default createHashRouter([
     {
@@ -16,5 +17,9 @@ export default createHashRouter([
     {
         path: '/publisher/:id',
         element: withNavBar(PublisherArticles)
+    },
+    {
+        path: '*',
+        element: withNavBar(NotFound)
     }
 ])
